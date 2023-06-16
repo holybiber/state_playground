cd8029620b1971e79b8632b6e3fe63f714f16f61
holybiber <samuel@holydevelopers.net>
2023-06-16 15:41:14 +0200

    App state management using listeners
    InheritedWidget, ChangeNotifier and ListenableBuilder playing together.

--- lib/main.dart
+++ lib/main.dart
@@ -1,130 +1,117 @@
 import 'package:flutter/material.dart';
 
-/*void main() {
-  runApp(const MaterialApp(
-      home: InheritedData(
-          data: Data(value: 'test'),
-          child: Column(
-            children: [
-              Example(),
-              WidgetC(),
-            ],
-          ))));
-}*/
+// ChangeNotifier is a mixin class and you can either extend or mix it
+class CounterState extends ChangeNotifier {
+  int _increment = 0;
+  int _decrement = 0;
+  int get incrementsCount => _increment;
+  int get decrementsCount => _decrement;
+
+  void increase() {
+    _increment++;
+    notifyListeners();
+  }
+
+  void decrease() {
+    _decrement++;
+    notifyListeners();
+  }
+}
+
+class InheritedCounter extends InheritedWidget {
+  final CounterState counterState;
+  const InheritedCounter(
+      {super.key, required this.counterState, required super.child});
+
+  static InheritedCounter of(BuildContext context) {
+    final i = context.dependOnInheritedWidgetOfExactType<InheritedCounter>();
+    assert(i != null, "No InheritedCounter found above in the tree.");
+    return i!;
+  }
+
+  @override
+  bool updateShouldNotify(covariant InheritedCounter oldWidget) {
+    return counterState != oldWidget.counterState;
+  }
+}
+
 void main() {
-  runApp(const MaterialApp(
-    home: Example(),
+  runApp(MaterialApp(
+    home: InheritedCounter(
+      counterState: CounterState(),
+      child: const CounterApp(),
+    ),
   ));
 }
 
-class InheritedData extends InheritedWidget {
-  final Data data;
-  const InheritedData({
-    super.key,
-    required this.data,
-    required super.child,
-  });
-
-  static InheritedData of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<InheritedData>()!;
-  }
-
-  @override
-  bool updateShouldNotify(covariant InheritedData oldWidget) {
-    return data.value != oldWidget.data.value; // Include here all dependencies
-  }
-}
-
-class WidgetWithData extends StatelessWidget {
-  final String value;
-  const WidgetWithData({super.key, required this.value});
+class CounterApp extends StatelessWidget {
+  const CounterApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return InheritedData(
-      data: Data(value: 'WidgetWithData'),
-      child: const WidgetA(),
+    return Scaffold(
+        appBar: AppBar(title: const AppTitle()), body: const AppBody());
+  }
+}
+
+class AppTitle extends StatelessWidget {
+  const AppTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final state = InheritedCounter.of(context).counterState;
+    // Rebuilds its children when CounterState uses notifyListeners()
+    return ListenableBuilder(
+        listenable: state,
+        builder: (context, _) {
+          final totalChanges = state.incrementsCount + state.decrementsCount;
+          return Text('Counter changes: $totalChanges');
+        });
+  }
+}
+
+class AppBody extends StatelessWidget {
+  const AppBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final state = InheritedCounter.of(context).counterState;
+
+    return Center(
+      child: Column(mainAxisSize: MainAxisSize.min, children: [
+        // The counter text at the center of the screen
+        const BodyText(),
+
+        // Some spacing
+        const SizedBox(height: 10),
+
+        // The two tappable buttons
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          // increases the counter and calls notifyListeners()
+          ElevatedButton(
+              onPressed: state.increase, child: const Text('Increment')),
+          const SizedBox(width: 30),
+          // decreases the counter and calls notifyListeners()
+          ElevatedButton(
+              onPressed: state.decrease, child: const Text('Decrement'))
+        ])
+      ]),
     );
   }
 }
 
-class WidgetA extends StatelessWidget {
-  const WidgetA({super.key});
-
-  @override
-  Widget build(BuildContext context) => const WidgetB();
-}
-
-class WidgetB extends StatelessWidget {
-  const WidgetB({super.key});
-
-  @override
-  Widget build(BuildContext context) => const WidgetC();
-}
-
-class WidgetC extends StatelessWidget {
-  const WidgetC({super.key});
+class BodyText extends StatelessWidget {
+  const BodyText({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text(InheritedData.of(context).data.value);
+    final state = InheritedCounter.of(context).counterState;
+
+    return ListenableBuilder(
+        listenable: state,
+        builder: (context, _) {
+          final diff = state.incrementsCount - state.decrementsCount;
+          return Text('The count is $diff');
+        });
   }
-}
-
-class Example extends StatefulWidget {
-  const Example({super.key});
-
-  @override
-  State<Example> createState() => _ExampleState();
-}
-
-class _ExampleState extends State<Example> {
-  var data = const Data(value: 'bla');
-
-  @override
-  Widget build(BuildContext context) {
-    return InheritedData(
-        data: data,
-        child: Column(
-          children: [
-            ElevatedButton(
-                onPressed: () => setState(() {
-                      data = Data(value: 'blub');
-                    }),
-                child: const Text('Rebuild')),
-            SubExample(),
-            Text(data.value),
-            WidgetA()
-          ],
-        ));
-  }
-}
-
-class SubExample extends StatefulWidget {
-  const SubExample({super.key});
-
-  @override
-  State<SubExample> createState() => _SubExampleState();
-}
-
-class _SubExampleState extends State<SubExample> {
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    debugPrint('The InheritedWidget changed!');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final data = InheritedData.of(context).data;
-    debugPrint('data: ${data.value}');
-    return Container(
-        color: Colors.blueAccent,
-        child: Row(children: [Text('Helloooo'), Text(data.value)]));
-  }
-}
-
-class Data {
-  final String value;
-  const Data({required this.value});
 }
