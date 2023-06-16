83f3403ff161604ebb68e0bea81e130e53c58dab
holybiber <samuel@holydevelopers.net>
2023-06-16 15:10:34 +0200

    Playing with InheritedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,130 @@
 import 'package:flutter/material.dart';
 
 /*void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: Center(
+  runApp(const MaterialApp(
+      home: InheritedData(
+          data: Data(value: 'test'),
           child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [Text('Press the button'), CounterApp()],
-          ),
-        ),
-      ),
-    ),
-  );
+            children: [
+              Example(),
+              WidgetC(),
+            ],
+          ))));
 }*/
-
 void main() {
   runApp(const MaterialApp(
-      home: WidgetA(
-    child: WidgetB(),
-  )));
+    home: Example(),
+  ));
 }
 
-class CounterApp extends StatefulWidget {
-  const CounterApp({super.key});
+class InheritedData extends InheritedWidget {
+  final Data data;
+  const InheritedData({
+    super.key,
+    required this.data,
+    required super.child,
+  });
+
+  static InheritedData of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<InheritedData>()!;
+  }
 
   @override
-  State<CounterApp> createState() {
-    return _CounterAppState();
+  bool updateShouldNotify(covariant InheritedData oldWidget) {
+    return data.value != oldWidget.data.value; // Include here all dependencies
   }
 }
 
-class _CounterAppState extends State<CounterApp> {
-  var counter = 0;
-  var visible = false;
+class WidgetWithData extends StatelessWidget {
+  final String value;
+  const WidgetWithData({super.key, required this.value});
 
   @override
   Widget build(BuildContext context) {
-    final text = visible ? 'Set to invisible' : 'Set to visibled';
-
-    return Column(
-      children: [
-        Text('Visible = $visible'),
-        const SizedBox(height: 10),
-        ElevatedButton(
-          onPressed: () => setState(() {
-            visible = !visible;
-            counter++;
-          }),
-          child: Text('$counter'),
-        ),
-      ],
+    return InheritedData(
+      data: Data(value: 'WidgetWithData'),
+      child: const WidgetA(),
     );
   }
 }
 
-class WidgetA extends StatefulWidget {
-  final Widget child;
-  const WidgetA({super.key, required this.child});
+class WidgetA extends StatelessWidget {
+  const WidgetA({super.key});
 
   @override
-  State<WidgetA> createState() => _WidgetAState();
-}
-
-class _WidgetAState extends State<WidgetA> {
-  @override
-  Widget build(BuildContext context) {
-    debugPrint('WidgetA rebuild');
-    return Column(
-      children: [
-        ElevatedButton(
-            onPressed: () => setState(() {}), child: const Text('Press me')),
-        widget.child
-      ],
-    );
-  }
+  Widget build(BuildContext context) => const WidgetB();
 }
 
 class WidgetB extends StatelessWidget {
   const WidgetB({super.key});
 
   @override
+  Widget build(BuildContext context) => const WidgetC();
+}
+
+class WidgetC extends StatelessWidget {
+  const WidgetC({super.key});
+
+  @override
   Widget build(BuildContext context) {
-    debugPrint('WidgetB rebuild');
-    return Container();
+    return Text(InheritedData.of(context).data.value);
   }
+}
+
+class Example extends StatefulWidget {
+  const Example({super.key});
+
+  @override
+  State<Example> createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  var data = const Data(value: 'bla');
+
+  @override
+  Widget build(BuildContext context) {
+    return InheritedData(
+        data: data,
+        child: Column(
+          children: [
+            ElevatedButton(
+                onPressed: () => setState(() {
+                      data = Data(value: 'blub');
+                    }),
+                child: const Text('Rebuild')),
+            SubExample(),
+            Text(data.value),
+            WidgetA()
+          ],
+        ));
+  }
+}
+
+class SubExample extends StatefulWidget {
+  const SubExample({super.key});
+
+  @override
+  State<SubExample> createState() => _SubExampleState();
+}
+
+class _SubExampleState extends State<SubExample> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    debugPrint('The InheritedWidget changed!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final data = InheritedData.of(context).data;
+    debugPrint('data: ${data.value}');
+    return Container(
+        color: Colors.blueAccent,
+        child: Row(children: [Text('Helloooo'), Text(data.value)]));
+  }
+}
+
+class Data {
+  final String value;
+  const Data({required this.value});
 }
